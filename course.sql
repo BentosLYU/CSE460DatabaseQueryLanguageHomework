1c330f2aba8b94ebfd6a5230af99277f691ab070
QinxinTian <aptsubl@gmail.com>
2017-11-04 01:53:49 -0400

    This is for the project 2, problem 2.

---
+++ course.sql
@@ -0,0 +1,32 @@
+/*2.1*/
+select *
+from student
+where sno not in (
+	select sno
+	from enroll, course
+	where dept='CSE'
+);
+
+/*2.2*/
+select student.sno, GPA
+from student left join (
+	select sno, sum(grade)as GPA
+	from enroll
+	group by sno
+) t on student.sno = t.sno;
+
+/*2.3*/
+select dept, count(*) as numCourse
+from course
+where cno in (
+	select t1.cno
+	from (
+		select course.cno, sum(case when course.dept=student.dept then 1 else -1 end) as num
+		from enroll, student, course
+		where enroll.sno = student.sno
+			and enroll.cno = course.cno
+		group by course.cno
+	) as t1
+	where num <= 0
+) as t2
+group by dept;
